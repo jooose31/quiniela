0865bdc154fd11d8a6741189dc93e225e7705d87
jooose31 <josed@galileo.edu>
2018-05-27 00:44:28 -0600

    base de datos

---
+++ quiniela.sql
@@ -0,0 +1,42 @@
+CREATE TABLE equipos(
+  ide integer,
+  puntos integer,
+  pais char(20),
+  grupo char(1),
+  bandera varchar(100),
+  primary key (id)
+);
+
+CREATE TABLE partidos(
+  idp varchar(100),
+  lugar varchar(100),
+  fechahora datetime,
+  ideu integer,
+  goleu integer,
+  ided integer,
+  goled integer,
+  fase varchar(100),
+  primary key(idp),
+  foreign key(ideu) references equipos,
+  foreign key(ided) references equipos,
+);
+
+CREATE TABLE participantes(
+  email varchar(100),
+  pass varchar(100),
+  puntos integer,
+  nombre varchar(100),
+  primary key (emailp)
+);
+
+CREATE TABLE quiniela(
+  idq varchar(20),
+  ge1 integer,
+  ge2 integer,
+  puntos integer,
+  idp varchar(100),
+  email varchar(100),
+  primary key(email,idp,idq),
+  foreign key(email) references participantes,
+  foreign key (idp) references partidos
+)
